c3712f20a4e601a63db75cbd258da76191c2cdc8
anilgehllot <anilgehlot.edu2k4@gmail.com>
2025-02-10 00:03:35 +0530

    completed till M10

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { connectDB } = require('./src/Database/db');
+const userRouter = require('./src/Controllers/users');
+const productrouter = require('./src/Controllers/Products');
+const app = express();
+
+require('dotenv').config({
+    path: './src/config/.env'
+});
+const port = process.env.PORT || 8080;
+const url = process.env.db_url;
+
+app.listen(port, async() => {
+
+  try{
+    await connectDB(url);
+    console.log(`Server is running on port ${port}`);
+  }
+  catch(error){
+    console.error(error);
+  }
+});
+
+app.use('/auth',userRouter)
+app.use('/product',productrouter)
+
+app.get('/', (req, res) => {    
+    res.send('welcome anil!');
+});
+
